d50feba833f1189b5592105b46994160c3ba74f9
Demmens <60469606+Demmens@users.noreply.github.com>
2020-06-07 22:44:43 +0100

    Update gm.js

--- commands/gm.js
+++ commands/gm.js
@@ -22,83 +22,103 @@ class GmCommand extends Command {
 				if (role[1].name == args.name){
 					if (role[1].hexColor == RP_COLOUR){return message.channel.send('A game of that name already exists.');}
 					return message.channel.send('You may not use that name.');
-				}
+				}			
+			}
+			//Check all the members roles
+			for (let role of us.roles.cache){
+				let isGM = 0;
 				//Check that they have the GM role
-				if (role[1].name == "GM"){
-					let hasRl = 0;
-					for(let mem of role[1].members){
-						if (mem[1] == us){ hasRl = 1; }
-					}
-					if (hasRl == 0){return message.channel.send('You must have the GM role to use that command. Ask the roleplay rep if you wish to run a game.')}
+				if (role[1].name == "GM"){isGM = 1;}
+				//Check they're not currently in a game
+				if (role[1].hexColor == RP_COLOUR){
+					return message.channel.send('You are already a part of a game. You must leave your current game before you can create one.')
 				}
+			}
+
+			if (isGM == 0){
+				return message.channel.send('You must have the GM role to use that command. Ask the roleplay rep if you wish to run a game.')
 			}
 
 			if (args.name == ''){
 				return message.channel.send('Please specify the name of your game.');
-			} else{
-				
-				//Create Role
-				let newRole = await gld.roles.create({
-					data: {
-						name: args.name,
-						color: RP_COLOUR
-					}
-				})
-
-				//give creator the role
-				message.member.roles.add(newRole);
-				//Create Corresponding Channels
-				let categ = await gld.channels.create(args.name, {
-					type: 'category',
-					permissionOverwrites:[
-						{
-							id: newRole.id,
-							allow: ['VIEW_CHANNEL']
-						},
-						{
-							id: gld.roles.everyone,
-							deny: ['VIEW_CHANNEL']
-						}
-					],
-				});
-				//Create Text Channel
-				gld.channels.create(args.name, {
-					type: 'text',
-					permissionOverwrites:[
-						{
-							id: newRole.id,
-							allow: ['VIEW_CHANNEL']
-						},
-						{
-							id: gld.roles.everyone,
-							deny: ['VIEW_CHANNEL']
-						}
-					],
-					parent: categ,
-				});
-				//Create Voice Channel
-				gld.channels.create(args.name, {
-					type: 'voice',
-					permissionOverwrites:[
-						{
-							id: newRole.id,
-							allow: ['VIEW_CHANNEL']
-						},
-						{
-							id: gld.roles.everyone,
-							deny: ['VIEW_CHANNEL']
-						}
-					],
-					parent: categ,
-				});
-
-				return message.channel.send('Created your game: \''.concat(args.name,'\'.'));
 			}
+			
+			//Create Role
+			let newRole = await gld.roles.create({
+				data: {
+					name: args.name,
+					color: RP_COLOUR
+				}
+			})
 
+			//give creator the role
+			us.roles.add(newRole);
+			//Create Corresponding Channels
+			let categ = await gld.channels.create(args.name, {
+				type: 'category',
+				permissionOverwrites:[
+					{
+						id: newRole.id,
+						allow: ['VIEW_CHANNEL']
+					},
+					{
+						id: gld.roles.everyone,
+						deny: ['VIEW_CHANNEL']
+					}
+				],
+			});
+			//Create Text Channel
+			gld.channels.create(args.name, {
+				type: 'text',
+				permissionOverwrites:[
+					{
+						id: newRole.id,
+						allow: ['VIEW_CHANNEL']
+					},
+					{
+						id: gld.roles.everyone,
+						deny: ['VIEW_CHANNEL']
+					}
+				],
+				parent: categ,
+			});
+			//Create Voice Channel
+			gld.channels.create(args.name, {
+				type: 'voice',
+				permissionOverwrites:[
+					{
+						id: newRole.id,
+						allow: ['VIEW_CHANNEL']
+					},
+					{
+						id: gld.roles.everyone,
+						deny: ['VIEW_CHANNEL']
+					}
+				],
+				parent: categ,
+			});
+
+			return message.channel.send('Created your game: \''.concat(args.name,'\'.'));
 		} else if (args.command == 'help'){
-			return message.channel.send('/gm create [name] - Makes channels for a game\n/gm rename [new name] - Renames the channels\n/gm remove - Removes your channels')
+			return message.channel.send('/gm create [name] - Makes channels for a game\n/gm rename [new name] - Renames the channels\n/gm remove - Removes your channels\n/gm leave - Leaves your current game')
+		} else if (args.command == 'leave'){
+			let gameRole;
+			//Check all the members roles
+			for (let role of us.roles.cache){
+				let isGM = 0;
+				//Check that they have the GM role
+				if (role[1].name == "GM"){isGM = 1;}
+				//Check they're not currently in a game
+				if (role[1].hexColor == RP_COLOUR){
+					gameRole = role[1];
+				}
+			}
+			if (isGM == 1){
+				return message.channel.send('You may not leave your game if you are a GM. Instead use \'/gm remove\' to remove your game.');
+			}
+			us.roles.remove(gameRole);
+			return message.channel.send('Successfully left your game.');
 		}
 	}
 }
-
 module.exports = GmCommand;
