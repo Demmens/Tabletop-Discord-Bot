2739fd58339ce7ac7ed6c4defe03bad1e20c0fad
Demmens <60469606+Demmens@users.noreply.github.com>
2020-06-07 16:31:15 +0100

    Update gm.js

--- commands/gm.js
+++ commands/gm.js
@@ -6,26 +6,26 @@ class GmCommand extends Command {
 			aliases: ["gm"],
 			args: [
 				{id: "command", type: "string", default: "help", match: "content"},
-				{id: "name", type: "string", default: "", match: "content"}
+				{id: "name", type: "string", default: 'asdf', match: "content"}
 			],
 			description: "Manages channels for your roleplay game"
 		});
 	}
 	exec(message, args) {
 		if (args.command == 'create') {
-			if (args.name == ''){
+			if (args.name == 'asdf'){
 				return message.channel.send('Please specify the name of your game');
 			} else{
 				message.guild.roles.create({
 					data: {
-						name: args.message,
+						name: args.name,
 						color: 'GOLD'
 					}
 				})
 					.then(console.log)
 					.catch(console.error);
 
-				return message.channel.send('Created your game: \''.concat(args.message,'\''));
+				return message.channel.send('Created your game: \''.concat(args.name,'\''));
 			}
 		}
 	}
