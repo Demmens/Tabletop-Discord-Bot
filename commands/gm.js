deb760e798265632ec06cc7531bd6f34826383a5
Demmens <60469606+Demmens@users.noreply.github.com>
2020-06-07 16:00:33 +0100

    Created first draft of gm.js

---
+++ commands/gm.js
@@ -0,0 +1,25 @@
+const { Command } = require("discord-akairo");
+
+class EchoCommand extends Command {
+	constructor() {
+		super("gm", {
+			aliases: ["gm", "createrpg"],
+			args: [{id: "message", type: "string", default: "Please specify the name of your game", match: "content"}],
+			description: "Creates channels for your roleplay game"
+		});
+	}
+	exec(message, args) {
+		guild.roles.create({
+			data: {
+				name: args.message,
+				color: 'YELLOW'
+			}
+		})
+			.then(console.log)
+			.catch(console.error);
+
+		return message.channel.send(args.message);
+	}
+}
+
+module.exports = EchoCommand;
