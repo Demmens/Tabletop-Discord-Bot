def824df1f4515d19b3dd7c40b5256ff4345ccbc
Demmens <60469606+Demmens@users.noreply.github.com>
2020-09-10 11:24:37 +0100

    Added Newcomer role stuff

--- listeners/reactRoleAdd.js
+++ listeners/reactRoleAdd.js
@@ -30,6 +30,22 @@ class ReactRoleAddListener extends Listener {
 					}
 				} 
 			}
+			else if (message.channel.id == 719204772590256159){
+				if (message.content.startsWith("**First time players**")){
+					let member = await message.guild.members.fetch(user.id);
+					let found_role = false;
+					for (let [_,role] of message.guild.roles.cache){
+
+						if (role.name == "Newcomer" && messageReaction.emoji.name == "YesVote"){
+							member.roles.add(role);
+							found_role = true;
+						}
+					}
+					if (!found_role) {
+						messageReaction.remove(user);
+					}
+				}
+			}
 		}
 	}
 }
