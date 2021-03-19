dc71f77d3381bb66e20d53a0465d3b758fb08f79
Demmens <60469606+Demmens@users.noreply.github.com>
2021-03-19 23:31:07 +0000

    Update welcome.js

--- listeners/welcome.js
+++ listeners/welcome.js
@@ -13,7 +13,7 @@ class WelcomeListener extends Listener {
 
 	exec(member) {
 		if (!this.client.testMode) {
-			member.guild.systemChannel.send(`Welcome to the **${member.guild.name}** server, ${member}! Go to the ${message.guild.channels.cache.get('769131551828869130').toString()} channel to gain access to the various parts of the society!`);
+			member.guild.systemChannel.send(`Welcome to the **${member.guild.name}** server, ${member}! Go to the ${member.guild.channels.cache.get('769131551828869130').toString()} channel to gain access to the various parts of the society!`);
 		}
 	}
 }
